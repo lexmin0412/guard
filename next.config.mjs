c26100b2b21632d680bc685f505b84b173b1eb14
lexmin0412 <zhangle_dev@outlook.com>
2024-07-22 22:46:46 +0800

    chore: 允许外部跨域请求

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	basePath: '/guard',
-	reactStrictMode: false
+	reactStrictMode: false,
+	async headers() {
+		return [
+			{
+				source: "/api/:path*",
+				headers: [
+					{
+						key: "Access-Control-Allow-Origin",
+						value: "*", // Set your origin
+					},
+					{
+						key: "Access-Control-Allow-Methods",
+						value: "GET, POST, PUT, DELETE, OPTIONS",
+					},
+					{
+						key: "Access-Control-Allow-Headers",
+						value: "Content-Type, Authorization",
+					},
+				],
+			},
+		];
+	}
 };
 
 export default nextConfig;
